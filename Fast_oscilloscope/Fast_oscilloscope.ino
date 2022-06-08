53c257f585cc7fe257373d44c5036dab3bbee328
whatbee <77581924+whatbee@users.noreply.github.com>
2022-06-08 20:27:39 +0100

    Update Fast_oscilloscope.ino
    Added stuff from amanda ghassaei's project to get the free running mode working

--- Fast_oscilloscope/Fast_oscilloscope.ino
+++ Fast_oscilloscope/Fast_oscilloscope.ino
@@ -11,22 +11,53 @@
 
 
 void setup() {
+  cli();//disable interrupts
   
+  //set up continuous sampling of analog pin 0
+  
+  //clear ADCSRA and ADCSRB registers
+  ADCSRA = 0;
+  ADCSRB = 0;
+  
+  ADMUX = 0;//Clear ADMUX register
+  ADMUX |= (1 << REFS0); //set reference AVcc - comment this out to use AREF
+  ADMUX |= (1 << ADLAR); //left align ADC
+  //since I'm reading A0, I don't need to specifiy which analog pin I want to read from (0 is default)
+  
+  ADCSRA |= (1 << ADPS2) | (1 << ADPS0); //set ADC clock with 32 prescaler- 16mHz/32=500kHz
+  ADCSRA |= (1 << ADATE); //enabble auto trigger
+  ADCSRA |= (1 << ADIE); //enable interrupts when measurement complete
+  ADCSRA |= (1 << ADEN); //enable ADC
+  ADCSRA |= (1 << ADSC); //start ADC measurements
+  
+  sei();//enable interrupts
+
+  Serial.begin(9600);
+  
+/*  
 DIDR0 = 0b11111100; // turn off digital buffer, leave reserved bits as 0
 DIDR1 = 0x00; // turn off digital buffer
-//ADCSRA = 0b01100111; // prescale 64 (250kHz), disable ADC interrupt, start auto ADC
-ADCSRA = 0b10100111; // prescale 32(500kHz), disable ADC interrupt, start auto ADC
-//ADCSRA = 0b00100111; // prescale 16(1MHz), disable ADC interrupt, start auto ADC
 ADCSRB = 0x00; // trigger source free running mode
 ACSR =  0x01;
-ADMUX = 0b00000100; // select ADC0, left adjust, use AREF
-
+//ADMUX = 0b00000100; // select ADC0, left adjust, use AREF
+ADMUX = 0b00000110; // select ADC0, left adjust, use AVcc
+//ADCSRA = 0b01100111; // prescale 64 (250kHz), disable ADC interrupt, start auto ADC
+//ADCSRA = 0b00100111; // prescale 16(1MHz), disable ADC interrupt, start auto ADC
+ADCSRA = 0b10101111; // prescale 32(500kHz), enable ADC interrupt, start auto ADC
 Serial.begin(9600);
+*/
 }
 
+ISR(ADC_vect){
+Serial.print(ADCL);
+Serial.print(",");
+Serial.println(ADCH);
+}
+
+
 void loop() {
-  uint8_t result = ADCH; // top 8 bits of ADC
-  Serial.println(result);
-  delay(100);
+ // uint8_t result = ADCH; // top 8 bits of ADC
+  //Serial.println(result);
+  //delay(100);
 
 }
